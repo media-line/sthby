dc700e1c9583541b5acf3dd2012a1a17e4e0adb1
rakatanui <grafgarrsky@gmail.com>
2016-05-18 17:41:24 +0300

    Revert "18/05/2016"
    This reverts commit 162c0cefbec14be87b461061fd8509f99b457ae6.

--- templates/sth/html/com_virtuemart/cart/order_form.php
+++ templates/sth/html/com_virtuemart/cart/order_form.php
@@ -1,30 +1,28 @@
 <?php
 ?>
-
-<h2 class="form-cart">Оформление заказа</h2>
-<form id="cart-form" class="form-horizontal" role="form" action="<?php /*тут будет код выбора в зависимости от того, что выбрано в селекте */?>">
+<form class="form-horizontal" role="form">
 		<div class="form-group form-cart">
 				<label for="inputFIO" class="col-sm-2 control-label cart-lable">ФИО</label>
 				<div class="col-sm-10">
-						<input type="text" class="form-control form-cart" id="inputFIO" required autocomplete>
+						<input type="text" class="form-control" id="inputFIO">
 				</div>
 		</div>
 		<div class="form-group form-cart">
 				<label for="inputPhone" class="col-sm-2 control-label cart-lable">Телефон</label>
 				<div class="col-sm-10">
-						<input type="tel" class="form-control form-cart" id="inputPhone" required autocomplete>
+						<input type="tel" class="form-control" id="inputPhone">
 				</div>
 		</div>
 		<div class="form-group form-cart">
 				<label for="inputEmail" class="col-sm-2 control-label cart-lable">Email</label>
 				<div class="col-sm-10">
-						<input type="email" class="form-control form-cart" id="inputEmail" required autocomplete>
+						<input type="email" class="form-control" id="inputEmail">
 				</div>
 		</div>
 		<div class="form-group form-cart">
 				<label for="inputPayMethod" class="col-sm-2 control-label cart-lable">Способ оплаты</label>
 				<div class="col-sm-10">
-						<select class="form-control form-cart" id="inputPayMethod">
+						<select class="form-control" id="inputPayMethod">
 								<option>Самовывоз</option>
 								<option>Оплата кредитной картой</option>
 						</select>
@@ -33,12 +31,12 @@
 		<div class="form-group form-cart">
 				<label for="inputComment" class="col-sm-2 control-label cart-lable">Комментарий</label>
 				<div class="col-sm-10">
-						<textarea class="form-control form-cart" rows="6" id="inputComment"></textarea>
+						<textarea class="form-control" rows="6" id="inputComment"></textarea>
 				</div>
 		</div>
-		<div class="form-group form-cart-btn">
+		<div class="form-group form-cart">
 				<div class="col-sm-offset-2 col-sm-10">
-						<button id="send" type="submit" class="btn btn-default btn-lg">Оформить</button>
+						<button type="submit" class="btn btn-default">Оформить</button>
 				</div>
 		</div>
 </form>
